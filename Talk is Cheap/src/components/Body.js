ba55277bedc47df3c35e8a64b44b52d24fdfa058
Jasdev2910 <jasdevvirdi987@gmail.com>
2023-08-31 23:34:45 +0530

    useState hook & filter added

--- Talk is Cheap/src/components/Body.js
+++ Talk is Cheap/src/components/Body.js
@@ -1,17 +1,26 @@
 import Card from "./Card";
 import search from "../assets/search.png";
 import { resList } from "../utils/mockData";
+import { useState } from "react";
 
 const Body = () => {
+    const [listOfRes, setListOfRes] = useState(resList);
+
     return(
         <div className="container">
+            <button className="filter-btn" onClick={() => {
+                const newResList = listOfRes.filter(
+                    (res) => res.info.avgRating > 4.3);
+                    console.log(newResList);
+                setListOfRes(newResList);
+            }}>Filter</button>
             <div className="search-container">
                 <input type="text" placeholder="Search Restauraunts"></input>
                 <button type="search"><img alt="search-icon" src={search}/></button>
             </div>
             <h2 className="heading">Restauraunts in Your Area</h2>
             <div className="card-container">
-            {resList.map((restaurant) => ( <Card key = {restaurant.info.id} resData = {restaurant} />))}
+            {listOfRes.map((restaurant) => ( <Card key = {restaurant.info.id} resData = {restaurant} />))}
             </div>
         </div>
     )
