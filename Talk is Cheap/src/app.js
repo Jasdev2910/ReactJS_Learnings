ce2c8efacf4b151afda90e3eff9525b4edc01e8b
Jasdev2910 <jasdevvirdi987@gmail.com>
2023-09-04 21:51:26 +0530

    Working Searchbar & Shimmer UI added

--- Talk is Cheap/src/app.js
+++ Talk is Cheap/src/app.js
@@ -3,15 +3,14 @@ import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
 
-
 const App = () => {
-    return(
-        <div>
-            <Header />
-            <Body />
-        </div>
-    )
-}
+  return (
+    <div>
+      <Header />
+      <Body />
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App/>);
+root.render(<App />);
--- Talk is Cheap/src/components/Body.js
+++ Talk is Cheap/src/components/Body.js
@@ -2,49 +2,83 @@ import Card from "./Card";
 import search from "../assets/search.png";
 import { resList } from "../utils/mockData";
 import { useState, useEffect } from "react";
+import Shimmer from "./Shimmer";
 
 const Body = () => {
-    const [listOfRes, setListOfRes] = useState(resList);
+  const [listOfRes, setListOfRes] = useState([]);
+  const [filteredrestaurant, setFilteredRestaurant] = useState([]);
+  const [searchText, setSerachText] = useState("");
 
-    useEffect( () => {
-        fetchData();
-        console.log("useeffect called");
-    }, []);
+  useEffect(() => {
+    fetchData();
+    console.log("useeffect called");
+  }, []);
 
-    const fetchData = async () => {
-    const response = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.196971&lng=81.289519&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+  const fetchData = async () => {
+    const response = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.196971&lng=81.289519&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
     const json = await response.json();
     console.log(json);
-    setListOfRes(json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
-        console.log(typeof(listOfRes));
-    }
+    setListOfRes(
+      json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+    setFilteredRestaurant(
+      json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+    console.log(typeof listOfRes);
+  };
 
-    console.log("body called");
+  console.log("body called");
 
-    return(
-        <div className="container">
-            <button className="filter-btn" onClick={() => {
-                const newResList = listOfRes.filter(
-                    (res) => res.info.avgRating > 4);
-                    console.log(newResList);
-                setListOfRes(newResList);
-            }}>Filter</button>
-            <div className="search-container">
-                <input type="text" placeholder="Search Restauraunts" required></input>
-                <button type="search" onClick={() => {
-                    const input = document.querySelector('input');
-                    const findRes = listOfRes.filter(
-                        (res) => res.info.name == input.value
-                    );
-                    setListOfRes(findRes);
-                }}><img alt="search-icon" src={search}/></button>
-            </div>
-            <h2 className="heading">Restauraunts in Your Area</h2>
-            <div className="card-container">
-            {listOfRes.map((restaurant) => ( <Card key = {restaurant.info.id} resData = {restaurant} />))}
-            </div>
-        </div>
-    )
-}
+  return listOfRes.length === 0 ? (
+    <Shimmer />
+  ) : (
+    <div className="container">
+      <button
+        className="filter-btn"
+        onClick={() => {
+          const newResList = filteredrestaurant.filter(
+            (res) => res.info.avgRating > 4
+          );
+          console.log(newResList);
+          setFilteredRestaurant(newResList);
+        }}
+      >
+        Filter
+      </button>
+
+      <div className="search-container">
+        <input
+          type="text"
+          placeholder="Search Restauraunts"
+          value={searchText}
+          onChange={(e) => {
+            setSerachText(e.target.value);
+          }}
+        />
+        <button
+          type="search"
+          onClick={() => {
+            const filteredList = listOfRes.filter((res) =>
+              res.info.name.toLowerCase().includes(searchText.toLowerCase())
+            );
+            setFilteredRestaurant(filteredList);
+          }}
+        >
+          <img alt="search-icon" src={search} />
+        </button>
+      </div>
+
+      <h2 className="heading">Restauraunts in Your Area</h2>
+
+      <div className="card-container">
+        {filteredrestaurant.map((restaurant) => (
+          <Card key={restaurant.info.id} resData={restaurant} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default Body;
