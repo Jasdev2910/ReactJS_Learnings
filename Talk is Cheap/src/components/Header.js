d4faf5e5f810e365d449f0d624f245a7cc1f9caf
Jasdev2910 <jasdevvirdi987@gmail.com>
2023-09-05 23:07:09 +0530

    Login button added

--- Talk is Cheap/src/components/Header.js
+++ Talk is Cheap/src/components/Header.js
@@ -1,20 +1,28 @@
 import Logo from "../assets/logo.png";
 import CartImg from "../assets/cart.png";
-
+import { useState } from "react";
 
 const Header = () => {
-    return(
-        <nav className="nav-bar">
-            <img src= {Logo} alt="logo"></img>
-            <h1>Food Hub</h1>
-            <ul>
-                <li>Offers</li>
-                <li>Sign In</li>
-                <li>Help</li>
-                <img className="cart-img" alt="cart-logo" src={CartImg} />
-            </ul>
-        </nav>
-    )
-}
+  const [btnName, setBtnName] = useState("Login");
+
+  return (
+    <nav className="nav-bar">
+      <img src={Logo} alt="logo"></img>
+      <h1>Food Hub</h1>
+      <ul>
+        <li>Offers</li>
+        <li
+          onClick={() => {
+            btnName === "Login" ? setBtnName("Logout") : setBtnName("Login");
+          }}
+        >
+          {btnName}
+        </li>
+        <li>Help</li>
+        <img className="cart-img" alt="cart-logo" src={CartImg} />
+      </ul>
+    </nav>
+  );
+};
 
 export default Header;
