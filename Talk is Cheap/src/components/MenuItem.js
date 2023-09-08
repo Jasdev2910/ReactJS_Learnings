ef77a72f7a120a994b8b718650b8fb2b31c50ea2
Jasdev2910 <jasdevvirdi987@gmail.com>
2023-09-08 23:10:08 +0530

    Menu Component updated

---
+++ Talk is Cheap/src/components/MenuItem.js
@@ -0,0 +1,26 @@
+const MenuItem = (props) => {
+  console.log(props);
+  return (
+    <div className="menu-container">
+      <div className="resInfo-menu-item-card">
+        <div className="resInfo-menu-item-card-deatils">
+          <h3>{props?.menu?.card?.info?.name}</h3>
+          <p>{"₹" + props?.menu?.card?.info?.price / 100}</p>
+        </div>
+        <div className="resInfo-menu-item-card-deatils">
+          <img
+            src={
+              "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/" +
+              props?.menu?.card?.info?.imageId
+            }
+            alt="img"
+          ></img>
+          <button>add+</button>
+        </div>
+      </div>
+      <div className="divider"></div>
+    </div>
+  );
+};
+
+export default MenuItem;
--- Talk is Cheap/src/components/RestaurantMenu.js
+++ Talk is Cheap/src/components/RestaurantMenu.js
@@ -1,26 +1,58 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import MenuItem from "./MenuItem";
 
 const RestaurantMenu = () => {
+  const [resInfo, setResInfo] = useState(null);
+
   useEffect(() => {
     fetchMenu();
-  });
+  }, []);
 
   const fetchMenu = async () => {
-    const data = await fetch("");
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=21.196971&lng=81.289519&restaurantId=257006&catalog_qa=undefined&submitAction=ENTER"
+    );
+    const json = await data.json();
+
+    console.log(json);
+    setResInfo(json.data);
+    Error("something went wrong");
   };
 
+  //   const { name, cuisines, areaName } =
+  //     resInfo?.data?.cards[0]?.card?.card?.info;
+  //   const { itemCards } =
+  //     resInfo.cards[2].groupedCard.cardGroupMap.REGULAR.cards[1].card.card;
+  //   console.log(itemCards);
   return (
-    <div>
-      <h1> name of restaurant </h1>
-      <p>cuisines</p>
-      <p>address</p>
-      <ul>
-        <h3>Menu</h3>
-        <li>idli</li>
-        <li>dosa</li>
-        <li>biriyani</li>
-        <li>rice</li>
-      </ul>
+    <div className="menu">
+      <div className="resInfo-container">
+        <div className="resInfo-card">
+          <div className="resInfo-card-header">
+            <div className="resInfo-card-header-name">
+              <h1>{resInfo?.cards[0]?.card?.card?.info?.name}</h1>
+              <p>{resInfo?.cards[0]?.card?.card?.info?.cuisines.join(", ")}</p>
+              <p>{resInfo?.cards[0]?.card?.card?.info?.areaName}</p>
+            </div>
+            <div className="resInfo-card-header-rating">
+              <h3>{resInfo?.cards[0]?.card?.card?.info.avgRating}</h3>
+            </div>
+          </div>
+          <div className="resInfo-card-footer">
+            <h3>{resInfo?.cards[0]?.card?.card?.info.costForTwoMessage}</h3>
+            <h3>
+              {resInfo?.cards[0]?.card?.card?.info.sla.deliveryTime} Minutes
+            </h3>
+          </div>
+        </div>
+        <div className="resInfo-menu">
+          {resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card?.itemCards
+            .splice(0)
+            .map((item) => (
+              <MenuItem menu={item} />
+            ))}
+        </div>
+      </div>
     </div>
   );
 };
