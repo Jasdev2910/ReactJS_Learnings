e1da789d0b880b9ec4d38cea85d505aa60b102ec
Jasdev2910 <jasdevvirdi987@gmail.com>
2023-09-16 00:27:42 +0530

    Tailwind Added

--- Talk is Cheap/src/components/MenuItem.js
+++ Talk is Cheap/src/components/MenuItem.js
@@ -3,22 +3,24 @@ import { MENU_ITEM_IMG } from "../utils/constants";
 const MenuItem = (props) => {
   console.log(props);
   return (
-    <div className="my-3 px-3">
-      <div className="flex justify-between my-5">
+    <div className="">
+      <div className="flex justify-between my-5 my-6 px-3 py-3">
         <div className="resInfo-menu-item-card-deatils">
           <h3>{props?.menu?.card?.info?.name}</h3>
           <p>{"₹" + props?.menu?.card?.info?.price / 100}</p>
         </div>
-        <div className="resInfo-menu-item-card-deatils">
+        <div className="relative">
           <img
             className="w-36 h-24 object-cover rounded-xl"
             src={MENU_ITEM_IMG + props?.menu?.card?.info?.imageId}
             alt="img"
           ></img>
-          <button>Add+</button>
+          <button className="absolute w-16 h-8 top-[80px] left-[40px] rounded-lg bg-slate-100 hover:-translate-y-1 transition text-sm">
+            Add+
+          </button>
         </div>
       </div>
-      <div className="divider"></div>
+      <div className="my-3 max-w-3xl h-[0.5px] bg-slate-300 "></div>
     </div>
   );
 };
